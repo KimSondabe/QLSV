0ca3754f087e958c896b5153aaa9016a92f8dd83
Vũ Kim Sơn <vukimsonmint@gmail.com>
2025-11-14 18:01:54 +0700

    Add files via upload

---
+++ qlss.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class SinhVien{
+    private : 
+        int MSSV; 
+        string name;
+        int age;
+        char gender; //Female (F) or Male (F)
+        int code; //Ma nganh (1 - 4)
+    public :
+        SinhVien(int m, string n, int a, char g, int c){
+            MSSV = m;
+            name = n;
+            age = a;
+            gender = g;
+            code = c;
+        }
+};
+
+
+int main(){
+    
+
+
+
+    return 0;
+}
